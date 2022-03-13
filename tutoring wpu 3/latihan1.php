61d80db18644ff114e735bca3e51510913289f8c
alghifarydaffa62 <88295192+alghifarydaffa62@users.noreply.github.com>
2022-03-13 18:40:43 +0700

    Add files via upload

---
+++ tutoring wpu 3/latihan1.php
@@ -0,0 +1,24 @@
+<?php
+// looping atau pengulangan
+
+
+// looping for
+for ($j = 0; $j < 5; $j++){
+    echo $j;
+    echo "<br>"; 
+}
+
+// looping while
+$i = 0;
+while ($i < 10) {
+    echo "Hello world <br>";
+$i++;
+}
+
+$k = 0;
+do {
+    echo $k;
+    $k++;
+}while ($k < 8);
+
+?>
---
+++ tutoring wpu 3/latihan3.php
@@ -0,0 +1,18 @@
+<?php 
+    // PENGKODISIAN
+    // if else
+    // if else if else
+    // ternary
+    // switch
+
+$x = 1;
+if($x < 10) {
+    echo "true";
+} else if ($x == 10) {
+    echo "Hasil Sama";
+}
+else {
+    echo "false"; 
+}
+
+?>
